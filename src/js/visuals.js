ae4479ed832d042253abadc17ba1ef3ddd597fdc
Jonas <jp.marcilio2@gmail.com>
2025-08-15 17:45:49 -0300

    att

--- src/js/visuals.js
+++ src/js/visuals.js
@@ -4,7 +4,7 @@ window.addEventListener("scroll", e => {
     if (window.pageYOffset > 200) {
         miniSolaresFigureEl.setAttribute("src", "/src/assets/img/mini_solares_looking_bottom.png")
     }
-    if (window.pageYOffset < 200) {
+    else {
         miniSolaresFigureEl.setAttribute("src", "/src/assets/img/mini_solares_looking_front.png")
     }
 })
